fc7a6587f1e6915b7017c48a24bb538b00b0d6bf
Sienna <remicknsisi@gmail.com>
2023-06-04 14:12:21 -0400

    added in session login/logout logic and authorize before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    before_action :authorize
 
-    def hello_world
-        session[:count] = (session[:count] || 0) + 1
-        render json: { count: session[:count] }
+    def authorize
+        return render json: { error: "Not Authorized" }, status: :unauthorized unless session.include? :user_id
+        @user = User.find_by(id: session[:user_id])
     end
 end
