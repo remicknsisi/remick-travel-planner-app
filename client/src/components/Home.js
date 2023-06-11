329266f8edb59cc199d5a5a0b9af516b14ffd9ad
Sienna <remicknsisi@gmail.com>
2023-06-11 12:25:00 -0400

    formatted out home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
 function Home() {
+    const history = useHistory()
+
 
   return (
-    <div>
-        This is the home component
+    <div className="home">
+        <div className="home-text">
+            Welcome to Vacation Planner!
+            <br/>
+            Click below or <Link to={`/signup`}>sign up</Link> to get started.
+            <br/>
+            <button className="button" onClick={() => history.push('/trips/new')}>Get Started 🌴</button>
+        </div>
     </div>
   );
 }
