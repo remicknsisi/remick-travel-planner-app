de1aff9f24cb8c371db43ebec30d5b9d97c3e532
Sienna <remicknsisi@gmail.com>
2023-06-11 13:41:17 -0400

    added in start of a nav and hooked up login and logout functionality

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useHistory, NavLink } from "react-router-dom";
+import { UserContext } from "../context/UserProvider.js";
 
 function Nav() {
+    const { currentUser, logout } = useContext(UserContext)
+    const history = useHistory()
 
-  return (
-    <div>
-        This is the nav component
-    </div>
-  );
+    function handleLogout(){
+        fetch("/logout",{
+            method: "DELETE"
+        })
+        .then(() => {
+            logout()
+            history.push('/login')
+        })
+    }
+
+    return (
+        <div className="nav">
+            <NavLink to="/" className="nav-link">Home</NavLink>
+            <NavLink to="/trips" className="nav-link">Trips</NavLink>
+            <NavLink to="/travelagents" className="nav-link">Find an Agent</NavLink>
+            {currentUser ? <button onClick={handleLogout}>Logout</button> : <button onClick={() => history.push('/login')}>Login</button>}
+        </div>
+        );
 }
 
 export default Nav;
