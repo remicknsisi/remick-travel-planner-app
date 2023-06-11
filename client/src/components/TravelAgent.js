4bf0050762c50f4b6865e3fdac4c0fcce6d98dea
Sienna <remicknsisi@gmail.com>
2023-06-11 12:43:42 -0400

    fleshed out TravelAgent component

--- client/src/components/TravelAgent.js
+++ client/src/components/TravelAgent.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 function TravelAgent({ travelAgent }) {
+    const {available, name, email, id} = travelAgent
+    const history = useHistory()
 
-  return (
-    <div>
-        <p>This is a Travel Agent component</p>
-    </div>
-  );
+    return (
+        <div>
+            <div className="text">
+                <img/>
+                Name: {name} | Contact: {email} | Available for Booking? {available ? '✅' : '❌'}
+                <button className="button" onClick={() => history.push(`/travelagents/${id}`)}>Learn More</button>
+            </div>
+        </div>
+    );
 }
 
 export default TravelAgent;
