35656fcd076817241f92675ed768faa8ade50087
Sienna <remicknsisi@gmail.com>
2023-07-19 20:09:51 -0400

    added ternary logic to all pages so you can view only when logged in

--- client/src/components/DisplayInspiration.js
+++ client/src/components/DisplayInspiration.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import DisplayActivities from "./DisplayActivities";
 import DisplayHotels from "./DisplayHotels";
 import DisplayLocations from "./DisplayLocations";
+import { UserContext } from "../context/UserProvider.js";
 
 function DisplayInspiration() {
     const [destinationsAreHidden, setDestinationsAreHidden] = useState(false)
     const [hotelsAreHidden, setHotelsAreHidden] = useState(true)
     const [activitiesAreHidden, setActivitiesAreHidden] = useState(true)
+    const { currentUser } = useContext(UserContext)
 
+    if (currentUser) {
     return (
         <div>
             <h1>Where will you go next?</h1>
@@ -27,7 +30,11 @@ function DisplayInspiration() {
             {hotelsAreHidden ? null : <DisplayHotels/>}
             {activitiesAreHidden ? null : <DisplayActivities/>}
         </div>
-    );
+    )} else {
+        return (
+            <p>Please login or signup to view this page.</p>
+        )
+    }
 }
 
 export default DisplayInspiration;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,9 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link, useHistory } from "react-router-dom";
-import { UserContext } from "../context/UserProvider.js";
 
 function Home() {
     const history = useHistory()
-    const { currentUser } = useContext(UserContext)
 
-    if (currentUser){
     return (
         <div className="home">
             <div className="welcome">
@@ -16,13 +13,12 @@ function Home() {
                 Click below or <Link to={`/signup`}>sign up</Link> to get started.
                 <br/>
                 <br/>
-                {currentUser.is_agent ? <button className="button" onClick={() => history.push('/trips/new')}>Get Started 🌴</button> : <button className="button" onClick={() => history.push('/inspiration')}>Get Started 🌴</button>}
+                <button className="button" onClick={() => history.push('/inspiration')}>Get Started 🌴</button>
                 <br/><br/>
                 <img className="home-img" src="https://images.unsplash.com/photo-1602002418816-5c0aeef426aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1674&q=80"/><img className="home-img" src="https://images.unsplash.com/photo-1597667554307-885851e705c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"/><img className="home-img" src="https://images.unsplash.com/photo-1602088113235-229c19758e9f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2067&q=80"/><img className="home-img" src="https://images.unsplash.com/photo-1548957175-84f0f9af659e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2082&q=80"/>
             </div>
         </div>
-        )
-    }
-}
+    )
+} 
 
 export default Home;
