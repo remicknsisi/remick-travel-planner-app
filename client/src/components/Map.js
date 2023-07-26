d1ba2b7be70ed0c6148a14d015e99170a5aef246
Sienna <remicknsisi@gmail.com>
2023-07-25 20:25:23 -0400

    added marker logic to map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,12 +1,34 @@
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
-import { useMemo } from "react";
+import { GoogleMap, Marker, InfoWindow, useLoadScript } from "@react-google-maps/api";
+import { useMemo, useState } from "react";
 import "../map.css"
 
 const Map = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY
   });
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+//   const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+  const [mapRef, setMapRef] = useState();
+  const [isOpen, setIsOpen] = useState(false);
+  const [infoWindowData, setInfoWindowData] = useState();
+  const markers = [
+    { address: "Address1", lat: 18.5204, lng: 73.8567 },
+    { address: "Address2", lat: 18.5314, lng: 73.8446 },
+    { address: "Address3", lat: 18.5642, lng: 73.7769 },
+  ];
+
+  const onMapLoad = (map) => {
+    setMapRef(map);
+    const google=window.google
+    const bounds = new google.maps.LatLngBounds();
+    markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+    map.fitBounds(bounds);
+  };
+
+  const handleMarkerClick = (id, lat, lng, address) => {
+    mapRef?.panTo({ lat, lng });
+    setInfoWindowData({ id, address });
+    setIsOpen(true);
+  };
 
   return (
     <div className="Map">
@@ -15,10 +37,31 @@ const Map = () => {
       ) : (
         <GoogleMap
           mapContainerClassName="map-container"
-          center={center}
-          zoom={10}
+          onLoad={onMapLoad}
+          onClick={() => setIsOpen(false)}
+        //   center={center}
+        //   zoom={10}
         >
-          <Marker position={{ lat: 18.52043, lng: 73.856743 }} />
+            {markers.map(({ address, lat, lng }, ind) => (
+            <Marker
+              key={ind}
+              position={{ lat, lng }}
+              onClick={() => {
+                handleMarkerClick(ind, lat, lng, address);
+              }}
+            >
+              {isOpen && infoWindowData?.id === ind && (
+                <InfoWindow
+                  onCloseClick={() => {
+                    setIsOpen(false);
+                  }}
+                >
+                  <h3>{infoWindowData.address}</h3>
+                </InfoWindow>
+              )}
+            </Marker>
+          ))}
+          {/* <Marker position={{ lat: 18.52043, lng: 73.856743 }} icon={"http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"} /> */}
         </GoogleMap>
       )}
     </div>
