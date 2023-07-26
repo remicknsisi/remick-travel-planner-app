a2741865224b12cf901be872d642732c185e5b2e
Sienna <remicknsisi@gmail.com>
2023-07-25 20:10:55 -0400

    added in google api key and map to appear on inspo page

--- client/src/components/DisplayInspiration.js
+++ client/src/components/DisplayInspiration.js
@@ -3,11 +3,13 @@ import DisplayActivities from "./DisplayActivities";
 import DisplayHotels from "./DisplayHotels";
 import DisplayLocations from "./DisplayLocations";
 import { UserContext } from "../context/UserProvider.js";
+import Map from "./Map";
 
 function DisplayInspiration() {
     const [destinationsAreHidden, setDestinationsAreHidden] = useState(false)
     const [hotelsAreHidden, setHotelsAreHidden] = useState(true)
     const [activitiesAreHidden, setActivitiesAreHidden] = useState(true)
+    const [mapsAreHidden, setMapsAreHidden] = useState(true)
     const { currentUser } = useContext(UserContext)
 
     if (currentUser) {
@@ -17,18 +19,27 @@ function DisplayInspiration() {
             <button onClick={() => {
                 setDestinationsAreHidden(!destinationsAreHidden)
                 setHotelsAreHidden(true)
+                setMapsAreHidden(true)
                 setActivitiesAreHidden(true)}}>View Destinations</button>
             <button onClick={() => {
                 setHotelsAreHidden(!hotelsAreHidden)
                 setActivitiesAreHidden(true)
+                setMapsAreHidden(true)
                 setDestinationsAreHidden(true)}}>View Hotels</button>
             <button onClick={() => {
                 setActivitiesAreHidden(!activitiesAreHidden)
                 setHotelsAreHidden(true)
+                setMapsAreHidden(true)
                 setDestinationsAreHidden(true)}}>View Activities</button>
+            <button onClick={() => {
+                setMapsAreHidden(!mapsAreHidden)
+                setDestinationsAreHidden(true)
+                setHotelsAreHidden(true)
+                setActivitiesAreHidden(true)}}>View Map</button>
             {destinationsAreHidden ? null : <DisplayLocations/>}
             {hotelsAreHidden ? null : <DisplayHotels/>}
             {activitiesAreHidden ? null : <DisplayActivities/>}
+            {mapsAreHidden ? null : <Map/>}
         </div>
     )} else {
         return (
